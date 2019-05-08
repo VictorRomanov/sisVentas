1fa02321333516e2b4cc2e4047fbfa758e427ce6
Victor <victor.romanov.r@gmail.com>
2019-05-07 22:19:38 -0400

    Creacion de Cliente y Proveedor

--- app/Http/Requests/PersonaFormRequest.php
+++ app/Http/Requests/PersonaFormRequest.php
@@ -24,7 +24,12 @@ class PersonaFormRequest extends Request
     public function rules()
     {
         return [
-            //
+        'nombre'=>'required|max:100',
+        'tipo_documento'=>'required|max:20',
+        'num_documento'=>'required|max:15',
+    	'direccion'=>'max:70',
+        'telefono'=>'max:15',
+        'email'=>'max:50'
         ];
     }
 }
